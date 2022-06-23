c31f5395aba9ebb78c7ee19276b6075141f5f88e
Priyanthan <priyanthan2@gmail.com>
2022-06-23 22:40:53 +0530

    PowerDrain

--- Source/Project_BC/Project_BCGameMode.cpp
+++ Source/Project_BC/Project_BCGameMode.cpp
@@ -12,4 +12,44 @@ AProject_BCGameMode::AProject_BCGameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+	// Base Decay Rate
+	DecayRate = 0.02f;
+
+	//Base Value for how frequantly to drain power
+	PowerDrainDelay = 0.25f;
+}
+
+void AProject_BCGameMode::BeginPlay()
+{
+	GetWorldTimerManager().SetTimer(PowerDrainTimer, this, &AProject_BCGameMode::DrainPowerOvertime, PowerDrainDelay, true);
+}
+
+float AProject_BCGameMode::GetDecay()
+{
+	return DecayRate;
+}
+
+void AProject_BCGameMode::DrainPowerOvertime()
+{
+	// Access the world to get to players
+	UWorld* World = GetWorld();
+	check(World);
+
+	// Go through all the character in the game
+	for (FConstControllerIterator It = World->GetControllerIterator(); It; ++It)
+	{
+		if (APlayerController* PlayerController = Cast<APlayerController>(*It))
+		{
+			if (AProject_BCCharacter* BatteryCharacter = Cast<AProject_BCCharacter>(PlayerController->GetPawn()))
+			{
+				if (BatteryCharacter->GetCurrentPower() > 0)
+				{
+					BatteryCharacter->UpdatePower(-PowerDrainDelay * DecayRate * (BatteryCharacter->GetInitialPower()));
+				}
+				
+			}
+		}
+	}
+
+
 }
--- Source/Project_BC/Project_BCGameMode.h
+++ Source/Project_BC/Project_BCGameMode.h
@@ -13,6 +13,31 @@ class AProject_BCGameMode : public AGameModeBase
 
 public:
 	AProject_BCGameMode();
+
+	// Start the powerDrain timer
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintPure, Category = "Power")
+	float GetDecay();
+
+	// How many Times per seconds to update characters power and chech game rules
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+	float PowerDrainDelay;
+
+	// Access the timer for recurring power draining
+	FTimerHandle PowerDrainTimer;
+
+protected:
+
+	// The rate at which character lose power % per seconds
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", meta = (BlueprintPortected = "true"))
+	float DecayRate;
+
+private:
+
+	//Drain power from character and update gameplay
+	void DrainPowerOvertime();
+
 };
 
 
